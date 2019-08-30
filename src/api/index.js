2274c889bf25a45df0fe33edb2fd6e4d64586106
Ashish Vagish <ashishvagish@outlook.in>
2019-08-30 18:50:26 +0530

    this time really removed client id

--- src/api/index.js
+++ src/api/index.js
@@ -9,10 +9,11 @@ while ((e = r.exec(q))) {
 }
 
 let expireTime;
+const loginUrl =
+    'https://accounts.spotify.com/authorize?client_id=<your client id past here>=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
 
 if (!hashParams.access_token) {
-    window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=<your client id past here>=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+    window.location.href = loginUrl;
 }
 
 const spotify = axios.create({
@@ -28,8 +29,7 @@ spotify.interceptors.request.use(config => {
 
     if (expireTime >= hashParams.expires_in) {
         localStorage.setItem('token_expire', new Date());
-        window.location.href =
-            'https://accounts.spotify.com/authorize?client_id=12b51ca6bc4a4076b9081ef3a7f784ca&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+        window.location.href = loginUrl;
     }
 
     return config;
