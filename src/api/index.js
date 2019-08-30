29873be094c54ad0aa8c1987857d4dc9a3aba2de
Ashish Vagish <ashishvagish@outlook.in>
2019-08-30 18:27:25 +0530

    removed client id

--- src/api/index.js
+++ src/api/index.js
@@ -12,7 +12,7 @@ let expireTime;
 
 if (!hashParams.access_token) {
     window.location.href =
-        'https://accounts.spotify.com/authorize?client_id=12b51ca6bc4a4076b9081ef3a7f784ca&scope=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
+        'https://accounts.spotify.com/authorize?client_id=<your client id past here>=playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20user-read-recently-played%20playlist-modify-private%20ugc-image-upload%20user-follow-modify%20user-follow-read%20user-library-read%20user-library-modify%20user-read-private%20user-read-email%20user-top-read%20user-read-playback-state&response_type=token&redirect_uri=http://localhost:3000/callback';
 }
 
 const spotify = axios.create({
